6cd0371dbe2d710fe3791a3c6e576b4ef38da913
Johannes Morgenroth <jm@m-network.de>
2017-10-03 19:38:25 +0200

    Send character presence

--- xmppbot.py
+++ xmppbot.py
@@ -60,25 +60,56 @@ class ChatBot(sleekxmpp.ClientXMPP):
         self.register_plugin('xep_0045')  # Multi-User Chat
         self.register_plugin('xep_0199')  # XMPP Ping
 
+        #self.register_plugin('xep_0012')  # Last Activity
+        self.register_plugin('xep_0186')  # Invisible Command
+        self.register_plugin('xep_0198')  # Stream Management
+        #self.register_plugin('xep_0352')  # Client State Indication
+
         self.resource = resource
         self.room = settings['room']
+        self.character_state = "available"
 
     def loop(self):
         if self.connect(reattempt=True):
             self.process(block=True)
 
+    def send_state(self, new_state=None):
+        if not new_state:
+            new_state = self.character_state
+
+        try:
+            if new_state == "available":
+                #self.plugin['xep_0352'].send_active()
+                #self.plugin['xep_0186'].set_visible()
+                self.send_presence()
+            else:
+                self.send_presence(pshow="unavailable")
+                #self.plugin['xep_0186'].set_invisible()
+                #self.plugin['xep_0352'].send_inactive()
+        except sleekxmpp.IqError:
+            pass
+
+        # set new state
+        self.character_state = new_state
+
+        # touch last activity indicator
+        #self.plugin['xep_0012'].set_last_activity()
+
     def start(self, event):
-        self.send_presence()
+        self.send_state()
         self.get_roster()
         self.plugin['xep_0045'].joinMUC(self.room, self.resource)
 
     def join(self):
+        self.send_state("available")
         self.plugin['xep_0045'].joinMUC(self.room, self.resource)
 
     def leave(self):
         self.plugin['xep_0045'].leaveMUC(self.room, self.resource)
+        self.send_state("unavailable")
 
     def talk(self, message):
+        #self.plugin['xep_0012'].set_last_activity()
         self.send_message(mto=self.room, mbody=message, mtype='groupchat')
 
 
